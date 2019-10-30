334b9b0f074d13a94b35253b933015690ac8c95b
Steve Goguen <steve.goguen@alliedbuilding.com>
2019-10-29 22:35:07 -0400

    Catalog progressively suggests keywords

---
+++ src/app/catalog.ts
@@ -0,0 +1,72 @@
+import { BehaviorSubject, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
+import * as faker from 'faker';
+
+export class Catalog {
+    private readonly products = generateCatalog();
+    public readonly search = new BehaviorSubject<string[]>([]);
+    public readonly foundProducts = this.search.pipe(map(terms => this.products.filter(p => hasKeywords(p, terms))));
+    public readonly keywords = combineLatest([this.foundProducts, this.search]).pipe(
+        map(([products, searchTerms]) => {
+            const keywords = new Set(getKeywords(products));
+            for (const t of searchTerms) {
+                keywords.delete(t);
+            }
+            return Array.from(keywords);
+        })
+    );
+
+    addSearch(searchTerm: string) {
+        const newSearch = this.search.value.map(i => i).concat(searchTerm);
+        this.search.next(newSearch);
+    }
+
+    removeSearch(searchTerm: string) {
+        const newSearch = this.search.value.filter(i => i !== searchTerm);
+        this.search.next(newSearch);
+    }
+}
+
+export function getKeywords(products: Product[]): string[] {
+    const keywords = new Set<string>();
+
+    for (const item of products) {
+        for (const keyword of getProductKeywords(item)) {
+            keywords.add(keyword);
+        }
+    }
+
+    return Array.from(keywords.keys()).sort();
+}
+
+function getProductKeywords(item: Product) {
+    return new Set<string>([
+        item.product.toLowerCase(),
+        item.color.toLowerCase(),
+        item.adjective.toLowerCase(),
+    ]);
+}
+
+export function hasKeywords(item: Product, searchKeywords: string[]): boolean {
+    const productKeywords = getProductKeywords(item);
+    return searchKeywords.every(k => productKeywords.has(k));
+}
+
+export function generateCatalog(): Product[] {
+    faker.seed(123);
+    const catalog = [];
+    for (let id = 1; id <= 100; id++) {
+        const product = faker.commerce.product();
+        const color = faker.commerce.color();
+        const adjective = faker.commerce.productAdjective();
+        catalog.push({ id, product, color, adjective });
+    }
+    return catalog;
+}
+
+interface Product {
+    id: number;
+    product: string;
+    color: string;
+    adjective: string;
+}
--- src/app/chips-autocomplete-example.ts
+++ src/app/chips-autocomplete-example.ts
@@ -1,22 +1,20 @@
-import { COMMA, ENTER } from "@angular/cdk/keycodes";
-import { Component, ElementRef, ViewChild } from "@angular/core";
-import { FormControl } from "@angular/forms";
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import {
   MatAutocompleteSelectedEvent,
   MatAutocomplete
-} from "@angular/material/autocomplete";
-import { MatChipInputEvent } from "@angular/material/chips";
-import { Observable, BehaviorSubject } from "rxjs";
-import { map, startWith, flatMap } from "rxjs/operators";
-import faker from "faker";
+} from '@angular/material/autocomplete';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { Catalog } from './catalog';
 
 /**
  * @title Chips Autocomplete
  */
 @Component({
-  selector: "chips-autocomplete-example",
-  templateUrl: "chips-autocomplete-example.html",
-  styleUrls: ["chips-autocomplete-example.css"]
+  selector: 'chips-autocomplete-example',
+  templateUrl: 'chips-autocomplete-example.html',
+  styleUrls: ['chips-autocomplete-example.css']
 })
 export class ChipsAutocompleteExample {
   visible = true;
@@ -25,40 +23,41 @@ export class ChipsAutocompleteExample {
   addOnBlur = true;
   separatorKeysCodes: number[] = [ENTER, COMMA];
   fruitCtrl = new FormControl();
-  filteredItems: Observable<string[]>;
-  selectedItems: string[] = ["Lemon"];
+  // selectedItems: string[] = [];
 
-  catalog = generateCatalog();
+  public readonly catalog = new Catalog();
+  public readonly filteredItems = this.catalog.keywords;
 
-  @ViewChild("fruitInput", { static: false }) fruitInput: ElementRef<
+  @ViewChild('fruitInput', { static: false }) fruitInput: ElementRef<
     HTMLInputElement
   >;
-  @ViewChild("auto", { static: false }) matAutocomplete: MatAutocomplete;
+  @ViewChild('auto', { static: false }) matAutocomplete: MatAutocomplete;
 
   constructor() {
-    this.filteredItems = this.fruitCtrl.valueChanges.pipe(
-      startWith(null),
-      map((fruit: string | null) =>
-        fruit ? this._filter(fruit) : this.getKeywords().slice()
-      )
-    );
+
+  }
+
+  get selectedItems() {
+    return this.catalog.search.value;
   }
 
   add(event: MatChipInputEvent): void {
     // Add fruit only when MatAutocomplete is not open
     // To make sure this does not conflict with OptionSelected Event
+    console.log({ event: 'add', data: event });
     if (!this.matAutocomplete.isOpen) {
       const input = event.input;
       const value = event.value;
 
       // Add our fruit
-      if ((value || "").trim()) {
-        this.selectedItems.push(value.trim());
+      if ((value || '').trim()) {
+        this.catalog.addSearch(value.trim());
+        // this.selectedItems.push(value.trim());
       }
 
       // Reset the input value
       if (input) {
-        input.value = "";
+        input.value = '';
       }
 
       this.fruitCtrl.setValue(null);
@@ -66,62 +65,14 @@ export class ChipsAutocompleteExample {
   }
 
   remove(item: string): void {
-    const index = this.selectedItems.indexOf(item);
-
-    if (index >= 0) {
-      this.selectedItems.splice(index, 1);
-    }
+    this.catalog.removeSearch(item);
   }
 
   selected(event: MatAutocompleteSelectedEvent): void {
-    this.selectedItems.push(event.option.viewValue);
-    this.fruitInput.nativeElement.value = "";
+    this.catalog.addSearch(event.option.viewValue);
+    this.fruitInput.nativeElement.value = '';
     this.fruitCtrl.setValue(null);
   }
-
-  getKeywords() {
-    const keywords = new Set<string>();
-    
-    for(var item of this.catalog) {
-      keywords.add(`${item.product.toLowerCase()}`);
-      keywords.add(`${item.color.toLowerCase()}`);
-      keywords.add(`${item.adjective.toLowerCase()}`);
-    }
-
-    return Array.from(keywords.keys()).sort();
-  }
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.getKeywords().filter(
-      fruit => fruit.toLowerCase().indexOf(filterValue) === 0
-    );
-  }
 }
 
 
-class Catalog {
-  private readonly products = generateCatalog();
-  public readonly search = new BehaviorSubject<string[]>([]);
-  public readonly foundProducts = this.search.pipe(
-    flatMap(terms => [terms])
-  );
-
-  addSearch(searchTerm: string) {
-    const newSearch = this.search.value.map(i => i).concat(searchTerm);
-    this.search.next(newSearch);
-  }
-}
-
-function generateCatalog() {
-  faker.seed(123);
-  let catalog = [];
-  for (let id = 1; id <= 20; id++) {
-    const product = faker.commerce.product();
-    const color = faker.commerce.color();
-    const adjective = faker.commerce.productAdjective();
-    catalog.push({ id, product, color, adjective });
-  }
-  return catalog;
-}
